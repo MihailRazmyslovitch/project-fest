cacad05f1da041a61849f70ae43d448f42a12dbc
Михаил Размыслович <Razmislovitchm@yandex.ru>
2024-07-11 10:33:16 +0300

    work first section and add js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,8 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.nav-menu'),
-    menuItem = document.querySelectorAll('.nav-menu__list-item'),
-    hamburger = document.querySelector('.hamburger');
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('nav-menu_active');
-    });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('nav-menu_active');
-        })
-    })
+document.querySelector('.btn-widget').addEventListener("click", () => {
+    document.querySelector('.widget').classList.toggle("widget__is-open")
+    document.querySelector('.btn-widget').classList.toggle("transform-rotate-180")
+    document.querySelector('widget__days::after').style.display = "block"
 })
+
+console.log(document.querySelector('widget__days::after'));
